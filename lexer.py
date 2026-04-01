676b5940e2b3d7d8418e9d977cb7eb88014ed12e
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-01 23:45:55 +0300

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -46,3 +46,34 @@ def next.token(self):
         regex = re.compile(pattern)
         match = regex.match(self source, self pos)
 
+        if match:
+          value = match.group(0)
+          start_col = self.col
+          self.pos = match.end()
+          self.col += len(value)
+
+          if token_type is None:
+            if '\n' in value:
+            self.line += value.count('\n')
+            self.col = 1
+            return self.next_token()
+
+       if token_type
+in [TokenType.NUMBER, TokenType.FLOAT_NUMBER]:
+                    value = float(value) if '.' in value else int(value)
+                elif token_type == TokenType.STRING:
+                    value = value[1:-1]
+                
+                return Token(token_type, value, self.line, start_col)
+        raise SyntaxError(f"Unknown char ate ln {self.line}, col {self.col}")
+    def tokenize(self):
+        tokens = []
+        while True:
+            token = self.next_token()
+            tokens.append(token)
+            if token.type == TokenType.EOF:
+                break
+        return tokens
+
+
+      
