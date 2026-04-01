914f545c42c9af4fe51521d60088a375a645af5f
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-01 23:23:47 +0300

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,36 @@
+from Enum import Enum, auto 
+from datac classes import dataclass
+#class
+class TokenWrite(Enum):
+  INT = auto()
+  FLOAT = auto()
+  INTR = auto()
+  SWITCH = auto()
+  CASE = auto()
+  DEFAULT = auto()
+  BREAK = auto()
+  PRINTF = auto()
+  SCANF = auto()
+  PLUS = auto()
+  MINUS = auto()
+  DIVISION = auto()
+  MULTIPLY = auto()
+  DIVIDE = auto()
+  ASSIGH = auto()
+  EQUALS = auto()
+  LPAPEN = auto()
+  RPAREN = auto()
+  SEMICOLON = auto()
+  COMMA = auto()
+  COLON = auto()
+  NUMBER = auto()
+  FLOAT_NUMBER = auto()
+  IDENTIFIER = auto()
+  STRING = auto()
+  EOF = auto()
+#burger
+@dataclass
+class Token:
+  type: TokenWrite
+  line: int
+  collum: int
